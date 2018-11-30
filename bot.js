8418d170298690db689032951b706bd907e71b27
3bker <44803452+3bker@users.noreply.github.com>
2018-11-30 17:12:57 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+﻿ const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("518078625728299008")
+setInterval(function() {
+channel.send(`Hello Guys I Want Credits Please Give Me Some Credits`);
+}, 30)
+})
+
+
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "1s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
